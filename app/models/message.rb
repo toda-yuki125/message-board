21b01dad9cbd67fb3ac18ab81785564c1403c679
yuki.toda <pyan.owl@gmail.com>
2017-06-04 16:11:57 +0000

    validation

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,5 @@
-class Message < ApplicationRecord
+class Message < ApplicationRecord 
+    validates :content, presence: true, length: { maximum: 255 }
+    #validates :content 文字をもたせる
+    #presence: true (カラを許さない)と length: { maximum: 255 } (255文字以内)
 end
